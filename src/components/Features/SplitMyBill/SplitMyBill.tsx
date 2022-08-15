6bcf4544eda5ac1b98f34c1ad06b79708ef84137
Nix Tay <niiiiiiiiiiix@users.noreply.github.com>
2022-08-16 02:54:29 +0800

    Add bunch of random features; to clean up

--- src/components/Features/SplitMyBill/SplitMyBill.tsx
+++ src/components/Features/SplitMyBill/SplitMyBill.tsx
@@ -4,8 +4,13 @@ import Greet from "./SimpleGreeter";
 import { properties } from "../../../StaticValues/properties";
 
 const SplitMyBill = () => {
+  const gstMultiplier = 1.07;
+  const serviceTaxMultiplier = 1.10;
   const [ participantCount, setParticipantCount ] = useState<string>("");
   const [ totalCost, setTotalCost ] = useState<string>("");
+  const [ individualItem, setIndividualItem] = useState<{itemName: string, itemCost: number, splitBy: number}[]>([]);
+  const [ participant, setParticipant] = useState<string[]>([]);
+  
   const handleInputChange = (event: ChangeEvent<HTMLInputElement>, type: string) => {
     const input = event.target.value;  
     if (type == properties.participantCount) {
@@ -30,9 +35,72 @@ const SplitMyBill = () => {
     }
   }
 
+  const valueAfterTax = (value: number) => {
+    return value * serviceTaxMultiplier * gstMultiplier
+  }
+
+  const AddParticipant = () => {
+    return (
+      <div>
+        <label htmlFor="">Name</label>
+        <input 
+          type="text"
+          required
+        />
+        <button onClick={() => setParticipant(prevItems => [...prevItems, "Bob"])}>
+          Add Participant
+        </button>
+        {participant.map((element, index) => {
+          return (
+            <div key={index}>
+              {element}
+            </div>
+          );
+        })}
+      </div>
+    )
+  }
+
+  const AddItem = () => {
+    return (
+      <div>
+        <label htmlFor="">Item Name</label>
+        <input 
+          type="text"
+          required
+        />
+        <label htmlFor="">Item Cost</label>
+        <input 
+          type="text"
+          required
+        />
+        <label htmlFor="">Split By</label>
+        <input 
+          type="text"
+          required
+        />
+        
+        <button onClick={() => setIndividualItem(prevItems => [...prevItems, {itemName: 'Kimchi', itemCost: 20, splitBy: 3}])}>
+          Add item
+        </button>
+        {individualItem.map((element, index) => {
+          return (
+            <div key={index}>
+              {element.itemName}
+              {element.itemCost}
+              {element.splitBy}
+            </div>
+          );
+        })}
+      </div>
+    )
+  }
+
   return (
     <div className="split-bill">
       <Greet name="random"/>
+      <AddParticipant />
+      <AddItem />
       {/* <GetValue stringValue="something"/> */}
       <div className="count-participants">
         {properties.numOfParticipants}
@@ -59,4 +127,4 @@ const SplitMyBill = () => {
   );
 };
 
-export default SplitMyBill;
+export default SplitMyBill
